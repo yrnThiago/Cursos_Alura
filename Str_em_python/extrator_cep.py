b41bd70100676b6927fa09c8060e89d9662f08fe
Thiago Ribeiro <65740833+yrnThiago@users.noreply.github.com>
2022-08-28 21:20:02 -0300

    Update extrator_cep.py

--- Str_em_python/extrator_cep.py
+++ Str_em_python/extrator_cep.py
@@ -1,6 +1,6 @@
 import re
 
-endereco = 'Rua José Eduardo Ferreira dos Santos, Jd. Cruzeiro do Sul, São Paulo, 12234-814'
+endereco = 'Rua github, Bairro gitgit, hubhub, 12345-678'
 padrao = re.compile('[0-9]{5}[-]?[0-9]{3}')
 busca = padrao.search(endereco)
 if busca:
