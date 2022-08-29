5ca13d3b2d4fce2aeff2dca8a0866ec6683e411b
Thiago Ribeiro <65740833+yrnThiago@users.noreply.github.com>
2022-08-28 21:18:46 -0300

    Update Funcionario.py

--- OO_2/Funcionario.py
+++ OO_2/Funcionario.py
@@ -20,8 +20,6 @@ class Caelum(Funcionario):
 
 
 class Alura(Funcionario):
-    # def mostrar_tarefas(self):
-    #   print('Fez muita coisa, Alurete!')
 
     def busca_perguntas_sem_resposta(self):
         print('Mostrando perguntas não respondidas do fórum')
