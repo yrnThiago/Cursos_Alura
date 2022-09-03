cd953b0fdb83b2947a7cad2cc93cdc96a4a71f28
yrnThiago <thiagodias_123@hotmail.com>
2022-09-03 15:52:45 -0300

    IO

--- Trabalhando_com_IO/principal.py
+++ Trabalhando_com_IO/principal.py
@@ -1,6 +1,10 @@
-arquivo_contatos = open('dados/contatos.csv', encoding='utf-8')
-
-conteudo = arquivo_contatos.readlines()
-
-for linha in conteudo:
-    print(linha, end='')
+try:
+    with open('dados/contatos-n.csv', encoding='utf-8') as arquivo_contatos:
+        for linha in arquivo_contatos:
+            print(linha, end='')
+except FileNotFoundError:
+    print('Arquivo não encontrado!')
+except PermissionError:
+    print('Sem permissão de escrita!')
+finally:
+    arquivo_contatos.close()
